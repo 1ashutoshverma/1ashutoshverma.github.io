aba3296d171fb0bf749329ae6c3371abd6a3f6a1
1ashutoshverma <ashu9236047613@gmail.com>
2023-10-11 00:53:09 +0530

    Created the home section

--- src/components/HomeSection/HomeSection.jsx
+++ src/components/HomeSection/HomeSection.jsx
@@ -4,7 +4,21 @@ import style from "./HomeSection.css"
 function HomeSection() {
     console.log(style)
     return (
-        <div id='home' style={{ backgroundColor: "green", height: "500px" }}>HomeSection</div>
+        <div id='home'>
+            <div id='left_home'>
+                <h1>Hi, I am</h1>
+                <h1>Ashutosh Verma</h1>
+                <h3>I am Full Stack Web Developer</h3>
+                <p>I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.</p>
+                {/* <div>
+                    <button>Check Resume</button>
+                    <button>Visit GitHub</button>
+                </div> */}
+            </div>
+            <div id='right_home'>
+                <img src="/Images/hero.jpg" alt="" />
+            </div>
+        </div>
     )
 }
 
