0588b17d98ca1b273c1b756f61ac297820277004
1ashutoshverma <ashu9236047613@gmail.com>
2023-10-25 12:57:49 +0530

    Added contact details

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -2,7 +2,30 @@ import React from 'react'
 
 function Contact() {
     return (
-        <div id='contact' style={{ backgroundColor: "green", height: "500px" }}>Contact</div>
+        <div id='contact' style={{ backgroundColor: "green", height: "500px" }}>
+            <div>
+                <div id="contact-github">
+                    <img src="" alt="" />
+                    <h5>Github</h5>
+                    <p>1ashutoshverma</p>
+                </div>
+                <div id="contact-linkedin">
+                    <img src="" alt="" />
+                    <h5>Linkedin</h5>
+                    <p>1ashutoshverma</p>
+                </div>
+                <div id="contact-phone">
+                    <img src="" alt="" />
+                    <h5>Email</h5>
+                    <p>ashu9236047613@gmail.com</p>
+                </div>
+                <div id="contact-email">
+                    <img src="" alt="" />
+                    <h5>Phone</h5>
+                    <p>+91 8756978783</p>
+                </div>
+            </div>
+        </div>
     )
 }
 
