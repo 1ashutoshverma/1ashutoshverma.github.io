826fe033c3a1f96674ae062562289eaf6f02a763
Ashutosh Verma <1ashutoshverma@gmail.com>
2024-10-20 18:27:57 +0000

    created timeline component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React, { useState } from 'react'
 import './App.css'
-import Navbar from './components/Navbar/Navbar'
-import HomeSection from './components/HomeSection/HomeSection'
-import AboutSection from './components/AboutSection/AboutSection'
-import Skills from './components/Skills/Skills'
-import Projects from './components/Projects/Projects'
-import Contact from './components/Contact/Contact'
+import Navbar from './sections/Navbar/Navbar'
+import HomeSection from './sections/HomeSection/HomeSection'
+import AboutSection from './sections/AboutSection/AboutSection'
+import Skills from './sections/Skills/Skills'
+import Projects from './sections/Projects/Projects'
+import Contact from './sections/Contact/Contact'
+import { TimelineComponent } from './components/Timeline/Timeline'
+import { Admin } from './utils/constants'
 
 function App() {
   return (
@@ -16,6 +18,7 @@ function App() {
         <AboutSection />
         <Skills />
         <Projects />
+        <TimelineComponent details={Admin.educationDetails} title={"Education"} />
         <Contact />
       </div>
     </div>
---
+++ src/sections/AboutSection/AboutSection.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './AboutSection.css';
+import { Admin } from '../../utils/constants';
+import { parseBoldText } from '../../utils/functions';
+
+function AboutSection() {
+    return (
+        <div id='about' className='about section'>
+            <h1>About Me</h1>
+            <p id="user-detail-intro">
+                {parseBoldText(Admin.aboutSection.about)}
+                <a href="#contact" className='contactMe'>{Admin.aboutSection.contact}</a>
+            </p>
+        </div>
+    );
+}
+
+export default AboutSection;
