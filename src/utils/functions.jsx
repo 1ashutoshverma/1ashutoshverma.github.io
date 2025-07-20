5f894a1e8948c6f6ac9b913a6688b1c5073be577
Ashutosh Verma <1ashutoshverma@gmail.com>
2025-07-20 21:47:12 +0530

    Update functions.jsx

--- src/utils/functions.jsx
+++ src/utils/functions.jsx
@@ -1,15 +1,25 @@
-export const downloadResume = () => {
-  const link = document.createElement("a");
-  link.download = "Ashutosh-Verma-Resume.pdf";
-  link.href = "/Ashutosh-Verma-Resume.pdf";
-  link.setAttribute("class", "resume-link-2");
-  link.click();
+// export const downloadResume = () => {
+//   const link = document.createElement("a");
+//   link.download = "Ashutosh-Verma-Resume.pdf";
+//   link.href = "/Ashutosh-Verma-Resume.pdf";
+//   link.setAttribute("class", "resume-link-2");
+//   link.click();
+//   window.open(
+//     "https://drive.google.com/uc?export=download&id=15JdBn34lKzaBECr6NkCT7RsnV5H2RG11",
+//     "_blank"
+//   );
+// };
 
-  window.open(
-    "https://drive.google.com/uc?export=download&id=15JdBn34lKzaBECr6NkCT7RsnV5H2RG11",
-    "_blank"
-  );
+export const downloadResume = () => {
+  window.open("/Ashutosh_Verma_Resume.pdf", "_blank");
+  const link = document.createElement("a");
+  link.href = "/Ashutosh_Verma_Resume.pdf";
+  link.download = "Ashutosh_Verma_Resume.pdf";
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
 };
+
 
 export const githubHandle = () => {
   window.open("https://github.com/1ashutoshverma", "_blank");
